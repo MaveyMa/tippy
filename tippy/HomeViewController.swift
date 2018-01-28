eece0b6210985cb76f96570c971abeb66cbd25fb
Mavey Ma <maveyma@gmail.com>
2018-01-28 15:04:45 -0800

    Settings page ViewPicker dynamically updates Tip and Total and remembers selected default

--- tippy/HomeViewController.swift
+++ tippy/HomeViewController.swift
@@ -14,6 +14,8 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    // Access UserDefaults
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,54 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        /**
+         * Summary: Overrides fading the Settings button after it's clicked
+         * Source: https://stackoverflow.com/questions/47754472/ios-uinavigationbar-button-remains-faded-after-segue-back
+         * Author: Code snippet by matt, Dec 15 '17 at 20:47
+         */
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.tintAdjustmentMode = .normal
+        self.navigationController?.navigationBar.tintAdjustmentMode = .automatic
+        
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+        // Get an Integer value.
+        let intValue = defaults.integer(forKey: "myIndexOfDefaultTipArray")
+        tipControl.selectedSegmentIndex = intValue
+        
+        let tipPercentages = [0.1, 0.15, 0.18, 0.20]
+        
+        //Convert billField string into a number
+        //If what's on the left of ?? is equal to nil,
+        // return 0 as the default value
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[intValue]
+        let total = bill + tip
+        
+        //A special syntax \(variableName) that's like concatenation
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
 
     @IBAction func onTap(_ sender: Any) {
         //When user taps anywhere outside of the billField, hide keyboard
@@ -31,7 +81,6 @@ class HomeViewController: UIViewController {
     }
     
     @IBAction func calculateTip(_ sender: Any) {
-        
         let tipPercentages = [0.1, 0.15, 0.18, 0.20]
         
         //Convert billField string into a number
@@ -46,15 +95,6 @@ class HomeViewController: UIViewController {
         totalLabel.text = String(format: "$%.2f", total)
     }
     
-    /**
-     * Summary: Overrides fading the Settings button after it's clicked
-     * Source: https://stackoverflow.com/questions/47754472/ios-uinavigationbar-button-remains-faded-after-segue-back
-     * Author: Code snippet by matt, Dec 15 '17 at 20:47
-     */
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.tintAdjustmentMode = .normal
-        self.navigationController?.navigationBar.tintAdjustmentMode = .automatic
-    }
+    
 }
 
--- tippy/SettingsViewController.swift
+++ tippy/SettingsViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-
+    //Access UserDefaults
+    let defaults = UserDefaults.standard
     @IBOutlet weak var defaultTipPickerView: UIPickerView!
-    
     var pickerData: [String] = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +26,22 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         
         // Input data into the Array:
         pickerData = ["10%", "15%", "18%", "20%"]
+        
+        let intValue = defaults.integer(forKey: "myIndexOfDefaultTipArray")
+        defaultTipPickerView.selectRow(intValue, inComponent: 0, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    /**
+     * Summary: Helper functions from PickerView tutorial
+     * Source: https://codewithchris.com/uipickerview-example/
+     * Author: Code snippet by Chris, Aug 26 '15
+     */
     
     // The number of columns of data
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -46,6 +57,21 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return pickerData[row]
     }
+    
+    // Catpure the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+        print("Row: \(row)")
+        print("Component: \(component)")
+        
+        // Set a String value for some key.
+        defaults.set(row, forKey: "myIndexOfDefaultTipArray")
+        
+        // Force UserDefaults to save.
+        defaults.synchronize()
+    }
+    
     
     
 
