04a4b96242a538ac47f541495c72e743ed337eb4
Mavey Ma <maveyma@gmail.com>
2018-01-29 13:35:25 -0800

    Updated indentation to 2 spaces as per raywenderlich's swift3 style guideline

--- tippy/HomeViewController.swift
+++ tippy/HomeViewController.swift
@@ -9,89 +9,89 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    @IBOutlet weak var clearButton: UIButton!
-    @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    // Access UserDefaults
-    let defaults = UserDefaults.standard
+  
+  @IBOutlet weak var clearButton: UIButton!
+  @IBOutlet weak var tipLabel: UILabel!
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var billField: UITextField!
+  @IBOutlet weak var tipControl: UISegmentedControl!
+  // Access UserDefaults
+  let defaults = UserDefaults.standard
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    let font = UIFont.systemFont(ofSize: 16)
+    tipControl.setTitleTextAttributes([NSAttributedStringKey.font: font],
+    for: .normal)
+    /**
+     * Summary: Overrides fading the Settings button after it's clicked
+     * Source: https://stackoverflow.com/questions/47754472/ios-uinavigationbar-button-remains-faded-after-segue-back
+     * Author: Code snippet by matt, Dec 15 '17 at 20:47
+     */
+    super.viewWillAppear(animated)
+    self.navigationController?.navigationBar.tintAdjustmentMode = .normal
+    self.navigationController?.navigationBar.tintAdjustmentMode = .automatic
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // This is a good place to retrieve the default tip percentage from UserDefaults
+    // and use it to update the tip amount
+    // Get an Integer value.
+    let intValue = defaults.integer(forKey: "myIndexOfDefaultTipArray")
+    tipControl.selectedSegmentIndex = intValue
+    let tipPercentages = [0.1, 0.15, 0.18, 0.20]
+    //Convert billField string into a number
+    //If what's on the left of ?? is equal to nil,
+    // return 0 as the default value
+    let bill = Double(billField.text!) ?? 0
+    let tip = bill * tipPercentages[intValue]
+    let total = bill + tip
     
-    override func viewWillAppear(_ animated: Bool) {
-        let font = UIFont.systemFont(ofSize: 16)
-        tipControl.setTitleTextAttributes([NSAttributedStringKey.font: font],
-                                                for: .normal)
-        /**
-         * Summary: Overrides fading the Settings button after it's clicked
-         * Source: https://stackoverflow.com/questions/47754472/ios-uinavigationbar-button-remains-faded-after-segue-back
-         * Author: Code snippet by matt, Dec 15 '17 at 20:47
-         */
-        super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.tintAdjustmentMode = .normal
-        self.navigationController?.navigationBar.tintAdjustmentMode = .automatic
-        
-        // This is a good place to retrieve the default tip percentage from UserDefaults
-        // and use it to update the tip amount
-        // Get an Integer value.
-        let intValue = defaults.integer(forKey: "myIndexOfDefaultTipArray")
-        tipControl.selectedSegmentIndex = intValue
-        
-        let tipPercentages = [0.1, 0.15, 0.18, 0.20]
-        
-        //Convert billField string into a number
-        //If what's on the left of ?? is equal to nil,
-        // return 0 as the default value
-        let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[intValue]
-        let total = bill + tip
-        
-        //A special syntax \(variableName) that's like concatenation
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-    }
+    //A special syntax \(variableName) that's like concatenation
+    tipLabel.text = String(format: "$%.2f", tip)
+    totalLabel.text = String(format: "$%.2f", total)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    billField.becomeFirstResponder()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+  }
+  
+  @IBAction func onTapClear(_ sender: UIButton) {
+    billField.text = "";
+    tipLabel.text = String(format: "$%.2f", "")
+    totalLabel.text = String(format: "$%.2f", "")
+  }
+  
+  @IBAction func calculateTip(_ sender: Any) {
+    let tipPercentages = [0.1, 0.15, 0.18, 0.20]
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        billField.becomeFirstResponder()
-    }
+    //Convert billField string into a number
+    //If what's on the left of ?? is equal to nil,
+    // return 0 as the default value
+    let bill = Double(billField.text!) ?? 0
+    let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+    let total = bill + tip
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
-
-    @IBAction func onTapClear(_ sender: UIButton) {
-        billField.text = "";
-        tipLabel.text = String(format: "$%.2f", "")
-        totalLabel.text = String(format: "$%.2f", "")
-    }
-    
-    @IBAction func calculateTip(_ sender: Any) {
-        let tipPercentages = [0.1, 0.15, 0.18, 0.20]
-        
-        //Convert billField string into a number
-        //If what's on the left of ?? is equal to nil,
-        // return 0 as the default value
-        let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
-        
-        //A special syntax \(variableName) that's like concatenation
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-    }
+    //A special syntax \(variableName) that's like concatenation
+    tipLabel.text = String(format: "$%.2f", tip)
+    totalLabel.text = String(format: "$%.2f", total)
+  }
+  
+  
 }
--- tippy/SettingsViewController.swift
+++ tippy/SettingsViewController.swift
@@ -9,61 +9,63 @@
 import UIKit
 
 class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
+  //Access UserDefaults
+  let defaults = UserDefaults.standard
+  @IBOutlet weak var defaultTipPickerView: UIPickerView!
+  var pickerData: [String] = [String]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    //Access UserDefaults
-    let defaults = UserDefaults.standard
-    @IBOutlet weak var defaultTipPickerView: UIPickerView!
-    var pickerData: [String] = [String]()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        // Connect data:
-        self.defaultTipPickerView.delegate = self
-        self.defaultTipPickerView.dataSource = self
-        
-        // Input data into the Array:
-        pickerData = ["10%", "15%", "18%", "20%"]
-        
-        let intValue = defaults.integer(forKey: "myIndexOfDefaultTipArray")
-        defaultTipPickerView.selectRow(intValue, inComponent: 0, animated: false)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // Do any additional setup after loading the view.
+    // Connect data:
+    self.defaultTipPickerView.delegate = self
+    self.defaultTipPickerView.dataSource = self
     
-    /**
-     * Summary: Helper functions from PickerView tutorial
-     * Source: https://codewithchris.com/uipickerview-example/
-     * Author: Code snippet by Chris, Aug 26 '15
-     */
-    // The number of columns of data
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
+    // Input data into the Array:
+    pickerData = ["10%", "15%", "18%", "20%"]
     
-    // The number of rows of data
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
+    let intValue = defaults.integer(forKey: "myIndexOfDefaultTipArray")
+    defaultTipPickerView.selectRow(intValue, inComponent: 0, animated: false)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  /**
+   * Summary: Helper functions from PickerView tutorial
+   * Source: https://codewithchris.com/uipickerview-example/
+   * Author: Code snippet by Chris, Aug 26 '15
+   */
+  // The number of columns of data
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  // The number of rows of data
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return pickerData.count
+  }
+  
+  // The data to return for the row and component (column) that's being passed in
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return pickerData[row]
+  }
+  
+  // Catpure the picker view selection
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    // This method is triggered whenever the user makes a change to the picker selection.
+    // The parameter named row and component represents what was selected.
     
-    // The data to return for the row and component (column) that's being passed in
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerData[row]
-    }
+    // Set a String value for some key.
+    defaults.set(row, forKey: "myIndexOfDefaultTipArray")
     
-    // Catpure the picker view selection
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        // This method is triggered whenever the user makes a change to the picker selection.
-        // The parameter named row and component represents what was selected.
-        
-        // Set a String value for some key.
-        defaults.set(row, forKey: "myIndexOfDefaultTipArray")
-        
-        // Force UserDefaults to save.
-        defaults.synchronize()
-    }
+    // Force UserDefaults to save.
+    defaults.synchronize()
+  }
+  
+  
 }
